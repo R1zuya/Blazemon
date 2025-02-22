f20ccd1042a4f532acf92d2f29fb1ce03ef6cf7b
R1zuya <rizuya.kituno@gmail.com>
2025-02-22 08:35:37 +0530

    Update battle.py

--- handlers/battle.py
+++ handlers/battle.py
@@ -1,1 +1,12 @@
+import random
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+from utils.helpers import calculate_damage
 
+async def battle(update: Update, context: CallbackContext):
+    user_pokemon = "Charmander"
+    wild_pokemon = "Squirtle"
+    damage = calculate_damage(user_pokemon, wild_pokemon)
+    await update.message.reply_text(f"{user_pokemon} attacked {wild_pokemon} and dealt {damage} damage!")
+
+battle_command = CommandHandler("battle", battle)
