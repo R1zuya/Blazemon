80ebdace13715a9d0d26fdd29f722977504dc1a7
R1zuya <rizuya.kituno@gmail.com>
2025-02-22 08:36:17 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,6 @@
+def calculate_damage(attacker, defender):
+    base_power = 50  # Default power
+    effectiveness = 1  # Implement type effectiveness later
+    damage = (base_power * effectiveness)
+    return damage
 
